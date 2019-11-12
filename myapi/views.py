16a386dfddd7c40c17ba3c5fc1fe456d6ba3f5d9
Karan Singla <karansingla.06@gmail.com>
2019-11-12 14:36:13 -0500

    change

--- myapi/views.py
+++ myapi/views.py
@@ -8,15 +8,21 @@ from django.http import JsonResponse
 from django.core import serializers
 from django.conf import settings
 import json
+import logging
 # Create your views here.
 
+logger = logging.getLogger(__name__)
 
-# https://gateway.watsonplatform.net/assistant/api/v1/workspaces/988d1327-d737-48c4-9e3e-a2e35c490db3/
 
 @api_view(["POST"])
 def BotProcessRequest(request):
     try:
+        logger.info("Request received from IBM watson")
         doc = request.data
+        logger.info("request type"+doc['intent'])
+
+        # send req to ibm to create
+        # https://gateway.watsonplatform.net/assistant/api/v1/workspaces/988d1327-d737-48c4-9e3e-a2e35c490db3/
 
         res = {"message": "success", "data":request.data}
         return JsonResponse(res)
