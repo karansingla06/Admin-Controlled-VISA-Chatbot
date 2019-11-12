21d6418b93da903ecd87c3e8ee3b41b11c2b6599
Karan Singla <karansingla.06@gmail.com>
2019-11-12 16:10:25 -0500

    changes

--- myapi/views.py
+++ myapi/views.py
@@ -24,7 +24,8 @@ def BotProcessRequest(request):
             url = 'https://gateway.watsonplatform.net/assistant/api/v1/workspaces/988d1327-d737-48c4-9e3e-a2e35c490db3/intents?version=2018-09-20'
             myobj = {'intent': doc['intent'], 'examples': doc['examples']}
             header= {"Content-Type":"application/json"}
-            x = requests.post(url, auth = ('apikey', 'kLqYFGYfATjuIYDhVmhAaBMZwQ8iz4iXqrfuk0rXL_0B'), json=myobj, headers=header)
+            x = requests.post(url, auth= ('apikey', 'kLqYFGYfATjuIYDhVmhAaBMZwQ8iz4iXqrfuk0rXL_0B'), data=myobj, headers=header)
+            logger.info('request response ---- : ', x.content)
             res = {"message": "success", "data": request.data}
         else:
             res = {"message": "failed", "data": request.data}
