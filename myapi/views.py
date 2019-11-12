0bf2b7b855d4a02a6f8c768d7b81a54679a7005a
Karan Singla <karansingla.06@gmail.com>
2019-11-12 13:01:12 -0500

    fist commit

---
+++ myapi/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from django.http import JsonResponse
+from django.core import serializers
+from django.conf import settings
+import json
+# Create your views here.
+
+
+# https://gateway.watsonplatform.net/assistant/api/v1/workspaces/988d1327-d737-48c4-9e3e-a2e35c490db3/
+
+
+@api_view(["POST"])
+def BotProcessRequest(request):
+    try:
+        print("ssssssssddddddddddd")
+        doc= request.data
+        res= {}
+        return JsonResponse(res)
+    except ValueError as e:
+        return Response(e.args[0],status.HTTP_400_BAD_REQUEST)
