03ff5f00c2c8906bd6d7f45fd2541432ac1418d5
Karan Singla <karansingla.06@gmail.com>
2019-11-12 15:33:23 -0500

    add examples for intetn

--- myapi/views.py
+++ myapi/views.py
@@ -22,9 +22,9 @@ def BotProcessRequest(request):
         doc = request.data
         if doc['request_type']== "intent":
             url = 'https://gateway.watsonplatform.net/assistant/api/v1/workspaces/988d1327-d737-48c4-9e3e-a2e35c490db3/'
-            myobj = {'intent': 'testing1'}
+            myobj = {'intent': doc['intent'], 'examples': doc['examples']}
             header= {"Content-Type":"application/json"}
-            x = requests.post(url, auth = ('apikey', 'Basic kLqYFGYfATjuIYDhVmhAaBMZwQ8iz4iXqrfuk0rXL_0B'), json=myobj, header=header)
+            x = requests.post(url, auth = ('apikey', 'Basic kLqYFGYfATjuIYDhVmhAaBMZwQ8iz4iXqrfuk0rXL_0B'), json=myobj, headers=header)
             res = {"message": "success", "data": request.data}
         else:
             res = {"message": "failed", "data": request.data}
