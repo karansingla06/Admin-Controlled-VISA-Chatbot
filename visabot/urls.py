b9367793e1e0682ac387eca0a87a046e1b9770f7
Karan Singla <karansingla.06@gmail.com>
2019-11-26 16:42:19 -0500

    changes

--- visabot/urls.py
+++ visabot/urls.py
@@ -19,11 +19,11 @@ from myapi import views
 from django.views.generic.base import TemplateView
 from django.conf.urls import url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-#from visabot.settings import STATIC_URL
+# from visabot.settings import STATIC_URL
 
 urlpatterns = [
     # url(r'', views.index, name="index"),
     url(r'^botrequest/', views.BotProcessRequest)
 ]
 
-urlpatterns += staticfiles_urlpatterns()
+# urlpatterns += staticfiles_urlpatterns()
